2a5dc9cf013742ceddd415281d88661e295a6771
Jack Higgins <jackhiggins07@gmail.com>
2013-08-09 11:29:11 +0100

    Javadoc

--- blocks/BlockLamp.java
+++ blocks/BlockLamp.java
@@ -6,22 +6,25 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemDye;
 import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-
-/*
- * Hi Lisa!
- */
 public class BlockLamp extends Block {
 	@SideOnly(Side.CLIENT)
 	private Icon lampOn;
 	@SideOnly(Side.CLIENT)
 	private Icon lampOff;
 	
+	/**
+	 *	Object constructor for the block.
+	 *	@param id 					Block id
+	 */
 	public BlockLamp(int id) {
 		super(id, Material.redstoneLight);
 
@@ -29,6 +32,9 @@ public class BlockLamp extends Block {
 		setCreativeTab(CreativeTabs.tabRedstone);
 	}
 
+	/**
+	 *	Adds the texture into the game from the assets folder.
+	 */
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerIcons(IconRegister register) {
@@ -36,6 +42,11 @@ public class BlockLamp extends Block {
 		lampOff = register.registerIcon(BlockInfo.TEXTURE_LOCATION + ":" + BlockInfo.LAMP_ICON_OFF);
 	}
 
+	/**
+	 *	Sets the texture for the block based on its Metadata.
+	 * 	@param side 				Side of the block to texture.
+	 * 	@param meta 				Metadata of the block.
+	 */
 	@SideOnly(Side.CLIENT)
 	@Override
 	public Icon getIcon(int side, int meta) {
@@ -46,6 +57,11 @@ public class BlockLamp extends Block {
 		return null;
 	}
 
+	/**
+	 *	Called when this block is placed in the world.
+ 	 * @param World 				World in which the block is activated.
+	 * @param x, y, z 				Co-ordinates of the block.
+	 */
 	@Override
 	public void onBlockAdded(World world, int x, int y, int z) {
 		if (!world.isRemote) {
@@ -57,6 +73,12 @@ public class BlockLamp extends Block {
 	}
 
 
+	/**
+	 *	Called when a block touching this block changes
+  	 * @param World 				World in which the block is activated.
+	 * @param x, y, z 				Co-ordinates of the block.
+	 * @param id 					Block id of the neighboring block
+	 */
 	@Override
 	public void onNeighborBlockChange(World world, int x, int y, int z, int id) {
 		if (!world.isRemote) {
@@ -71,6 +93,11 @@ public class BlockLamp extends Block {
 		}
 	}
 
+	/**
+	 *	Sets the light value of the block.
+ 	 * @param World 				World in which the block is activated.
+	 * @param x, y, z 				Co-ordinates of the block.
+	 */
 	@Override
 	public int getLightValue(IBlockAccess world, int x, int y, int z) {
 		int meta = world.getBlockMetadata(x, y, z);
@@ -84,12 +111,34 @@ public class BlockLamp extends Block {
 
 	/**
 	 * Ticks the block if it's been scheduled
+	 * @param World 				World in which the block is activated.
+	 * @param x, y, z 				Co-ordinates of the block.
+	 * @param player 				Player who right clicked the block.
 	 */
+	@Override
 	public void updateTick(World world, int x, int y, int z, Random random)
 	{
 		if (!world.isRemote && !world.isBlockIndirectlyGettingPowered(x, y, z))
 		{
 			world.setBlock(z, y, z, BlockInfo.LAMP_ID, 1, 2);
 		}
+	}
+
+	/**
+	 * Called upon block activation (right click on the block.)
+	 * @param World 				World in which the block is activated.
+	 * @param x, y, z 				Co-ordinates of the block.
+	 * @param player 				Player who right clicked the block.
+	 * @param side 					Side of the block clicked.
+	 * @param hitX, hitY, hitZ 		Where on the block was clicked.
+	 */
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		if (!world.isRemote) {
+			if (player.getHeldItem() != null && player.getHeldItem().getItem() instanceof ItemDye) {
+				System.out.println("Item: " + ItemDye.dyeColorNames[player.getHeldItem().getItemDamage()] + " dye.");
+			}
+		}
+		return true;
 	}
 }
--- blocks/Blocks.java
+++ blocks/Blocks.java
@@ -7,18 +7,21 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class Blocks {
 
-	public static Block lampOff;
-	public static Block lampOn;
+	public static Block lamp;
 
+	/**
+	 *	Initialises all the blocks in our mod
+	 */
 	public static void init() {
-		lampOff = new BlockLamp(BlockInfo.LAMP_ID);
-//		lampOn = new BlockLamp(BlockInfo.LAMP_ID+1, 1);
-		GameRegistry.registerBlock(lampOff, ItemLampBlock.class, BlockInfo.LAMP_KEY);
-//		GameRegistry.registerBlock(lampOn, ItemLampBlock.class, BlockInfo.LAMP_KEY);
+		lamp = new BlockLamp(BlockInfo.LAMP_ID);
+		GameRegistry.registerBlock(lamp, ItemLampBlock.class, BlockInfo.LAMP_KEY);
 	}
 
+	/**
+	 *	Adds names to all of our blocks in the mod.
+	 */
 	public static void addNames() {
-		LanguageRegistry.addName(lampOff, BlockInfo.LAMP_NAME);
-//		LanguageRegistry.addName(lampOn, BlockInfo.LAMP_NAME);
+		LanguageRegistry.addName(lamp, BlockInfo.LAMP_NAME);
+
 	}
 }
